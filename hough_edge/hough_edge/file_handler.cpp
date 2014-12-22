1f4f7c1d6510813bf08862a830a54790b566cc5e
moazzamak <moazzam2k@yahoo.com>
2014-12-22 15:55:56 +0100

    housekeeping

--- hough_edge/hough_edge/file_handler.cpp
+++ hough_edge/hough_edge/file_handler.cpp
@@ -7,14 +7,14 @@ cv::vector<std::string> FileHandler::list_files(std::string directory){
 	cv::vector<std::string> output;
 	
 	DIR *pDIR;
-        struct dirent *entry;
-		if( pDIR=opendir(directory.c_str()) ){
-                while(entry = readdir(pDIR)){
-                        if( strcmp(entry->d_name, ".") != 0 && strcmp(entry->d_name, "..") != 0 ){
-							output.push_back(entry->d_name);
-						}
-                }
-                closedir(pDIR);
-        }
-		return output;
+    struct dirent *entry;
+	if( pDIR=opendir(directory.c_str()) ){
+            while(entry = readdir(pDIR)){
+                    if( strcmp(entry->d_name, ".") != 0 && strcmp(entry->d_name, "..") != 0 ){
+						output.push_back(entry->d_name);
+					}
+            }
+            closedir(pDIR);
+    }
+	return output;
 }
